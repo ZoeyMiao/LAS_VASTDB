bd1dc7ba21131a1f4d79242d91f58e7504aad8ac
alicetorch <zmu@smith.edu>
2018-04-06 09:58:48 -0400

    Add files via upload

--- message_identify.js
+++ message_identify.js
@@ -5,16 +5,87 @@ wet land (0,45) - (100,70)
 coaster alley (0,70) - (100,100) */
 
 var allRows;
+var canvas;
+var context;
+var map;
+//show coordinates on map
+$(document).ready(function(){
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    map = new Image();
+    map.src = "/Auxiliary Files/Park_Map.jpg";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw map
+    // X location, Y location, Image width, Image height
+    context.drawImage(map, 0, 0, 700, 700);}
+); 
+
+
+var Marker = function () {
+    this.Sprite = new Image();
+    this.Sprite.src = "/Auxiliary Files/map-marker-hi.png"
+    this.Width = 35;
+    this.Height = 40;
+    this.XPos = 0;
+    this.YPos = 0;
+}
+var Markers = new Array();
 
 $.ajax({
   url: '/MC1 Data June 2015 V3/park-movement-Fri-FIXED-2.0.csv',
   dataType: 'text',
 }).done(processData);
 
+
 function processData(data) {
     allRows = data.split(/\r?\n|\r/);
     console.log(allRows[2]);
 }
+
+//append new candidate, push each candidate to markers array.
+function append(rowCells){
+    $("p").append(document.createTextNode(rowCells[1]));
+    $("p").append(document.createElement("br"));
+    var marker = new Marker();
+    marker.XPos = parseInt(rowCells[3]) * 7;
+    marker.YPos = parseInt(rowCells[4]) * 7;
+    console.log("add mark")
+    Markers.push(marker);
+}
+
+var draw = function () {
+    context.clearRect(0,0,canvas.width,canvas.height);
+    // Clear Canvas
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw map
+    // X location, Y location, Image width, Image height
+    context.drawImage(map, 0, 0, 700, 700);
+
+    // Draw markers
+    for (var i = 0; i < Markers.length; i++) {
+        var tempMarker = Markers[i];
+        // Draw marker
+        console.log(tempMarker.XPos);
+        context.drawImage(tempMarker.Sprite, tempMarker.XPos, tempMarker.YPos - 35, tempMarker.Width, tempMarker.Height);
+
+//        // Calculate postion text
+//        var markerText = "Postion (X:" + tempMarker.XPos + ", Y:" + tempMarker.YPos;
+
+        // Draw a simple box so you can see the position
+//        var textMeasurements = context.measureText(markerText);
+        context.fillStyle = "#666";
+        context.globalAlpha = 0.7;
+//        context.fillRect(tempMarker.XPos - (textMeasurements.width / 2), tempMarker.YPos - 15, textMeasurements.width, 20);
+        context.globalAlpha = 1;
+
+        // Draw position above
+        context.fillStyle = "#000";
+//        context.fillText(markerText, tempMarker.XPos, tempMarker.YPos);
+    }
+};
 
 function search(){
     console.log("skjdfhskdhfkj");
@@ -41,34 +112,30 @@ function search(){
             var y = parseInt(rowCells[4]);
             if(location == 'Kiddle Land'){
                 if (x > 70 && y < 45){
-                    $("p").append(document.createTextNode(rowCells[1]));
-                    $("p").append(document.createElement("br"));
+                    append(rowCells);
                 }
             }
             else if(location == 'Entry Corridor'){
                 if (x > 50 && x < 70 && y < 45){
-                    $("p").append(document.createTextNode(rowCells[1]));
-                    $("p").append(document.createElement("br"));
+                    append(rowCells);
                 }
             }
             else if(location == 'Tundra Land'){
                 if (x < 50 && y < 45){
-                    $("p").append(document.createTextNode(rowCells[1]));
-                    $("p").append(document.createElement("br"));
+                    append(rowCells);
                 }
             }
             else if(location == 'Wet Land'){
                 if (y < 70 && y > 45){
-                    $("p").append(document.createTextNode(rowCells[1]));
-                    $("p").append(document.createElement("br"));
+                    append(rowCells);
                 }
             }
             else if(location == 'Coaster Alley'){
                 if (y < 100 && y > 70){
-                    $("p").append(document.createTextNode(rowCells[1]));
-                    $("p").append(document.createElement("br"));
+                    append(rowCells);
                 }
             }
         }
-    }    
+    } 
+    draw();
 }
