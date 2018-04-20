7c3428f97b97235737601c55dcbf679ba950f633
Zoey Sun <zsun@smith.edu>
2018-04-20 10:57:40 -0400

    Add files via upload

--- message_identify.js
+++ message_identify.js
@@ -3,7 +3,6 @@ entry corridor (50,0) - (70,45)
 kiddle land (70,0) - (100,45)
 wet land (0,45) - (100,70)
 coaster alley (0,70) - (100,100) */
-<<<<<<< HEAD
 var resultTable;
 var resultdot;
 var context;
@@ -51,55 +50,6 @@ function append(rowCells){
     var marker = new Marker();
     marker.XPos = parseInt(rowCells["3"]) * 5;
     marker.YPos = parseInt(rowCells["4"]) * 5;
-=======
-
-var allRows;
-var canvas;
-var context;
-var map;
-//show coordinates on map
-$(document).ready(function(){
-    canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d");
-    map = new Image();
-    map.src = "/Auxiliary Files/Park_Map.jpg";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Draw map
-    // X location, Y location, Image width, Image height
-    context.drawImage(map, 0, 0, 700, 700);}
-);
-
-
-var Marker = function () {
-    this.Sprite = new Image();
-    this.Sprite.src = "/Auxiliary Files/map-marker-hi.png"
-    this.Width = 35;
-    this.Height = 40;
-    this.XPos = 0;
-    this.YPos = 0;
-}
-var Markers = new Array();
-
-$.ajax({
-  url: '/MC1 Data June 2015 V3/park-movement-Fri-FIXED-2.0.csv',
-  dataType: 'text',
-}).done(processData);
-
-
-function processData(data) {
-    allRows = data.split(/\r?\n|\r/);
-    console.log(allRows[2]);
-}
-
-//append new candidate, push each candidate to markers array.
-function append(rowCells){
-    $("p").append(document.createTextNode(rowCells[1]));
-    $("p").append(document.createElement("br"));
-    var marker = new Marker();
-    marker.XPos = parseInt(rowCells[3]) * 7;
-    marker.YPos = parseInt(rowCells[4]) * 7;
-    console.log("add mark")
     Markers.push(marker);
 }
 
@@ -112,12 +62,6 @@ var draw = function () {
     // X location, Y location, Image width, Image height
     context.drawImage(map, 0, 0, 500, 500);
     console.log(Markers);
-
-
-    // Draw map
-    // X location, Y location, Image width, Image height
-    context.drawImage(map, 0, 0, 700, 700);
-
     // Draw markers
     for (var i = 0; i < Markers.length; i++) {
         var tempMarker = Markers[i];
@@ -164,74 +108,4 @@ function search(){
     });
     draw();
 
-}
-
-
-//        // Calculate postion text
-//        var markerText = "Postion (X:" + tempMarker.XPos + ", Y:" + tempMarker.YPos;
-
-        // Draw a simple box so you can see the position
-//        var textMeasurements = context.measureText(markerText);
-        context.fillStyle = "#666";
-        context.globalAlpha = 0.7;
-//        context.fillRect(tempMarker.XPos - (textMeasurements.width / 2), tempMarker.YPos - 15, textMeasurements.width, 20);
-        context.globalAlpha = 1;
-
-        // Draw position above
-        context.fillStyle = "#000";
-//        context.fillText(markerText, tempMarker.XPos, tempMarker.YPos);
-    }
-};
-
-function search(){
-    console.log("skjdfhskdhfkj");
-    var timestamp = document.getElementById("timestamp").value;
-    var margin = document.getElementById("margin").value;
-    var location = document.getElementById("location").value;
-
-    var inputTime = new Date(timestamp);
-    var startTime = new Date(inputTime.getTime() - 1000*margin);
-    var endTime = new Date(inputTime.getTime() + 1000*margin);
-    console.log(inputTime);
-    console.log(startTime);
-    console.log(endTime);
-
-    //loop through csv files to filter
-    for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
-        var rowCells = allRows[singleRow].split(',');
-        var cellTime = new Date(rowCells[0]);
-        if (cellTime > endTime){
-            break;
-        }
-        if (cellTime > startTime && cellTime < endTime){
-            var x = parseInt(rowCells[3]);
-            var y = parseInt(rowCells[4]);
-            if(location == 'Kiddle Land'){
-                if (x > 70 && y < 45){
-                    append(rowCells);
-                }
-            }
-            else if(location == 'Entry Corridor'){
-                if (x > 50 && x < 70 && y < 45){
-                    append(rowCells);
-                }
-            }
-            else if(location == 'Tundra Land'){
-                if (x < 50 && y < 45){
-                    append(rowCells);
-                }
-            }
-            else if(location == 'Wet Land'){
-                if (y < 70 && y > 45){
-                    append(rowCells);
-                }
-            }
-            else if(location == 'Coaster Alley'){
-                if (y < 100 && y > 70){
-                    append(rowCells);
-                }
-            }
-        }
-    }
-    draw();
 }
